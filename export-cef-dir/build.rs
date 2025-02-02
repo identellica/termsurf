3b540d4e853f361b5b2f47faf4a5bed619c0bdc8
Bill Avery <wravery@gmail.com>
2025-02-01 18:31:25 -0800

    feat: turn export-cef-dir into a standalone utility

--- export-cef-dir/build.rs
+++ export-cef-dir/build.rs
@@ -1,10 +1,1 @@
-use std::{env, fs::File, io::Write, path::PathBuf};
-
-fn main() -> anyhow::Result<()> {
-    let cef_dir = env::var("DEP_CEF_DLL_WRAPPER_CEF_DIR")?;
-    eprintln!("DEP_CEF_DLL_WRAPPER_CEF_DIR: {cef_dir}");
-    let cef_dir_rs = PathBuf::from(env::var("OUT_DIR")?).join("cef_dir.rs");
-    let mut cef_dir_rs = File::create(cef_dir_rs)?;
-    writeln!(&mut cef_dir_rs, r#"const CEF_DIR: &str = r"{cef_dir}";"#)?;
-    Ok(())
-}
+fn main() {}
